9eacfbb74db9d20bf3fa9fdc7289befd2c1e2cd6
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-16 08:43:08 -0500

    Add postcss-import to the frontend

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -12,6 +12,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 });
 module.exports = {
   plugins: [
+    require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
