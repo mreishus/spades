fed262c0bf220e56fa5a147ba4b8b544c22eeaa2
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-16 09:53:49 -0500

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION=0.0.1
+
+# exit when any command fails
+set -e
+
+## Backend
+echo "Building backend..."
+cd backend
+docker build --tag=mreishus/spades-backend:$VERSION --tag=mreishus/spades-backend:latest .
+docker push mreishus/spades-backend:latest
+docker push mreishus/spades-backend:$VERSION
+cd ..
+
+## Frontend
+echo "Building frontend..."
+cd frontend
+docker build --tag=mreishus/spades-frontend:$VERSION --tag=mreishus/spades-frontend:latest .
+docker push mreishus/spades-frontend:latest
+docker push mreishus/spades-frontend:$VERSION
+cd ..
+
+echo "Done!"
