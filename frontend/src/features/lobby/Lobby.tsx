ed51ce1d92897842f936fe5217841def6a036e3b
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-19 02:58:34 +0000

    Lobby lists games

--- frontend/src/features/lobby/Lobby.tsx
+++ frontend/src/features/lobby/Lobby.tsx
@@ -3,14 +3,30 @@ import CreateRoomModal from "./CreateRoomModal";
 import Button from "../../components/basic/Button";
 import Container from "../../components/basic/Container";
 
+import useDataApi from "../../hooks/useDataApi";
+
 interface Props {}
 
 export const Lobby: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
+  const { isLoading, isError, data } = useDataApi("/be/api/rooms", null);
+
+  let roomItems = null;
+  if (data != null && data.data != null) {
+    roomItems = data.data.map((room: any) => (
+      <div key={room.id}>
+        id {room.id} name {room.name}
+      </div>
+    ));
+  }
+
   return (
     <Container>
       <div>
         this is the lobby
+        {isLoading && <div>Loading..</div>}
+        {isError && <div>Error..</div>}
+        {roomItems}
         <div>
           <Button onClick={() => setShowModal(true)}>Create Room</Button>
         </div>
