a9bc19aa413fc9c60c54733c67dc5f5ca3833315
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-24 20:56:24 -0500

    Lobby updates

--- frontend/src/features/lobby/Lobby.tsx
+++ frontend/src/features/lobby/Lobby.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from "react";
+import { Link } from "react-router-dom";
 import CreateRoomModal from "./CreateRoomModal";
 import Button from "../../components/basic/Button";
 import Container from "../../components/basic/Container";
@@ -28,23 +29,45 @@ export const Lobby: React.FC = () => {
   );
   const broadcast = useChannel("lobby:lobby", onChannelMessage);
 
+  const tdClass = "p-3 border-b border-gray-400";
+  const thClass = "p-3 border-b border-gray-400";
+
   let roomItems = null;
   if (data != null && data.data != null) {
     roomItems = data.data.map((room: Room) => (
-      <div key={room.id}>
-        id {room.id} name {room.name} slug {room.slug}
-      </div>
+      <tr key={room.id}>
+        <td className={tdClass}>{room.id}</td>
+        <td className={tdClass}>
+          <Link to={"/room/" + room.slug}>{room.name}</Link>
+        </td>
+        <td className={tdClass}>empty</td>
+        <td className={tdClass}>empty</td>
+        <td className={tdClass}>empty</td>
+        <td className={tdClass}>empty</td>
+      </tr>
     ));
   }
 
   return (
     <Container>
       <div>
-        this is the lobby
+        <h1 className="mb-4">Lobby</h1>
         {isLoading && <div>Loading..</div>}
         {isError && <div>Error..</div>}
-        {roomItems}
-        <div>
+        {roomItems && (
+          <table className="shadow rounded border bg-gray-100">
+            <thead>
+              <th className={thClass}>id</th>
+              <th className={thClass}>name</th>
+              <th className={thClass}>west</th>
+              <th className={thClass}>north</th>
+              <th className={thClass}>east</th>
+              <th className={thClass}>south</th>
+            </thead>
+            <tbody>{roomItems}</tbody>
+          </table>
+        )}
+        <div className="mt-4">
           <Button isPrimary onClick={() => setShowModal(true)}>
             Create Room
           </Button>
