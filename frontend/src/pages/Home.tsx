ce5869fa85d8cb145f0564fc905a8edd1d0a02d3
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-04 10:29:57 -0600

    Homepage -> Lobby

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import Container from "../components/basic/Container";
-import Card from "../components/basic/Card";
+import {Redirect} from "react-router";
+/* import Container from "../components/basic/Container"; */
+/* import Card from "../components/basic/Card"; */
 
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
   return (
-    <Container>
-      <Card className="mt-20 p-4 bg-gray-100 rounded-lg shadow-lg">
-        <div className="text-lg my-2">
-          StarSpades is still WIP, and not available. The current goal is to
-          have a minimally playable version ready on Nov 13, 2019.
-        </div>
-        <div>Last update: 2019-11-04.</div>
-      </Card>
-    </Container>
+    <Redirect to="/lobby" />
   );
+  /* return ( */
+  /*   <Container> */
+  /*     <Card className="mt-20 p-4 bg-gray-100 rounded-lg shadow-lg"> */
+  /*       <div className="text-lg my-2"> */
+  /*         StarSpades is still WIP, and not available. The current goal is to */
+  /*         have a minimally playable version ready on Nov 13, 2019. */
+  /*       </div> */
+  /*       <div>Last update: 2019-11-04.</div> */
+  /*     </Card> */
+  /*   </Container> */
+  /* ); */
 };
 export default Home;
