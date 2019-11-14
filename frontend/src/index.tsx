e4764d32f5caa659dbbe132d81cdce0b2fa3fcd7
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-14 11:02:23 -0600

    Add redux root reducer, store, and provider with HMR support

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./index.css";
-import App from "./app/App";
+
+//import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
 
+// From https://redux-toolkit.js.org/tutorials/advanced-tutorial
+const render = () => {
+  const App = require("./app/App").default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./app/App", render);
+}
+
 // Old
-ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM.render(<App />, document.getElementById("root"));
 
 // Concurrent Mode
 // @ts-ignore
