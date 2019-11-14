548a6e2e5f582550574c8a6f2e39a4fc18a10f3a
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-14 11:52:01 -0600

    Create AuthContext type

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,21 @@
 import { createContext } from "react";
 
-const AuthContext = createContext<any>(false);
+interface AuthContextType {
+  setAuthAndRenewToken: (
+    authTokenData: null | string,
+    renewTokenData: null | string
+  ) => void;
+  authToken: null | string;
+  renewToken: null | string;
+  logOut: any;
+}
+
+const defaultValue: AuthContextType = {
+  setAuthAndRenewToken: (_x, _y) => null,
+  authToken: null,
+  renewToken: null,
+  logOut: () => null
+};
+
+const AuthContext = createContext<AuthContextType>(defaultValue);
 export default AuthContext;
