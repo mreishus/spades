cf173d0ef1a97c782ac50a0f2e96ce7d246466c3
Matthew Reishus <mreishus@users.noreply.github.com>
2020-01-25 08:39:05 -0600

    Upgrade phoenix

--- backend/mix.exs
+++ backend/mix.exs
@@ -33,7 +33,7 @@ defmodule Spades.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.10"},
+      {:phoenix, "~> 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
