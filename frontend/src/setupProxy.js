b2b8d2a1776111942b23f866fd72c92d82e98d84
Matthew Reishus <mreishus@users.noreply.github.com>
2020-03-16 11:53:21 -0500

    Update http-proxy-middleware to 1.x

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,8 +1,9 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 module.exports = function(app) {
   app.use(
     "/be",
-    proxy({
+    createProxyMiddleware({
       target: "http://localhost:4000",
       changeOrigin: true,
       pathRewrite: { "^/be": "" }
