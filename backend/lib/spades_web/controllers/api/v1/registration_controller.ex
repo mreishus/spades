aecf004c368d07942c58e6cb985b520c82990077
Matthew Reishus <mreishus@users.noreply.github.com>
2020-03-16 11:17:26 -0500

    Email confirm links now signed, moved away from deprecated APIs

--- backend/lib/spades_web/controllers/api/v1/registration_controller.ex
+++ backend/lib/spades_web/controllers/api/v1/registration_controller.ex
@@ -30,27 +30,30 @@ defmodule SpadesWeb.API.V1.RegistrationController do
   end
 
   _ = """
-  Sends a confirmation e-mail to the user.
-
-  The user struct passed to the mailer will have the `:email` set to the
-  `:unconfirmed_email` value if `:unconfirmed_email` is set.
-
-  *** This is copied and modified from
+  *** Next two functions are copied and modified from
   ./lib/extensions/email_confirmation/phoenix/controllers/controller_callbacks.ex
   in the 'pow' library.
   REASON: Customize the url sent to include the front-end. ***
   """
 
+  @doc """
+  Sends a confirmation e-mail to the user.
+
+  The user struct passed to the mailer will have the `:email` set to the
+  `:unconfirmed_email` value if `:unconfirmed_email` is set.
+  """
   @spec send_confirmation_email(map(), Conn.t()) :: any()
-  defp send_confirmation_email(user, conn) do
-    url = confirmation_url(user.email_confirmation_token)
+  def send_confirmation_email(user, conn) do
+    url = confirmation_url(conn, user)
     unconfirmed_user = %{user | email: user.unconfirmed_email || user.email}
     email = PowEmailConfirmation.Phoenix.Mailer.email_confirmation(conn, unconfirmed_user, url)
 
     Pow.Phoenix.Mailer.deliver(conn, email)
   end
 
-  defp confirmation_url(token) do
+  defp confirmation_url(conn, user) do
+    token = PowEmailConfirmation.Plug.sign_confirmation_token(conn, user)
+
     Application.get_env(:spades, SpadesWeb.Endpoint)[:front_end_email_confirm_url]
     |> String.replace("{token}", token)
   end
