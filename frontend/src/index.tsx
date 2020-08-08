ae71956c31dba65f7843a4abc1cdce7919c19504
Matthew Reishus <mreishus@users.noreply.github.com>
2020-08-08 11:28:46 -0500

    Remove serviceworker
    I was getting "Unhandled Rejection (SecurityError): The operation is insecure."
    in firefox.
    
    See https://github.com/badges/shields/issues/3901

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,6 @@ import store from "./store";
 import "./index.css";
 
 //import App from "./app/App";
-import * as serviceWorker from "./serviceWorker";
 
 // From https://redux-toolkit.js.org/tutorials/advanced-tutorial
 const render = () => {
@@ -32,8 +31,3 @@ if (process.env.NODE_ENV === "development" && module.hot) {
 // Concurrent Mode
 // @ts-ignore
 //ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
