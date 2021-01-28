6d2f02a05048a9f7bf5abf59c302e66b4abc6adf
chrisstanford <chris.j.stanford@gmail.com>
2021-01-27 21:26:40 -0800

    added giantcard

---
+++ frontend/src/features/room/GiantCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useActiveCard } from "../../contexts/ActiveCardContext";
+import { getCurrentFaceSRC } from "./CardView";
+
+export const GiantCard = () => {
+    const activeCardAndLoc = useActiveCard();
+    const activeCard = activeCardAndLoc?.card
+    return (
+        <div className="h-full" 
+            style={{
+              backgroundImage: `url(${getCurrentFaceSRC(activeCard)})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+            }}
+          >
+        </div>
+    )
+  }
+  
+  export default GiantCard;
