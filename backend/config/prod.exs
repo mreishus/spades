625e4b37e4b9b8a0b1d7446706789d9348de1e17
Chris Stanford <chris.j.stanford@gmail.com>
2021-05-10 14:02:54 +0000

    server updates

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -10,10 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dragncards, DragnCardsWeb.Endpoint,
-	http: [port: 4000],
+  http: [port: 4000],
   url: [host: "dragncards.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json",
-	check_origin: ["//localhost", "//dragncards", "//dragncards.com", "//www.dragncards.com"]
+  check_origin: ["//localhost", "//dragncards", "//dragncards.com", "//www.dragncards.com"],
   # check_origin: [
   #   "//dragncards.com",
   #   "//api.dragncards.com",
