2a7fdc656ab7a4afb52f622457e2600171388775
Chris Stanford <chris.j.stanford@gmail.com>
2021-07-27 21:59:01 -0400

    token css fixes

--- frontend/src/features/room/Token.js
+++ frontend/src/features/room/Token.js
@@ -70,28 +70,23 @@ export const Token = React.memo(({
 
     return(
         <div
+        
             style={{
                 position: "absolute",
                 left: `${left}`,
                 top: `${top}`,
                 height: `25%`,
-                width: `35%`,
-                alignItems: "center",
                 zIndex: showButtons ? zIndex + 1 : "",
                 display: showButtons || amount!==0 ? "block" : "none",
-            }}
-        >
-            <p 
-                className="text-center text-sm"
+            }}>
+            <div
+                className="flex absolute text-white text-center w-full h-full items-center justify-center"
                 style={{
-                    position: "absolute",
-                    color: "white", 
                     textShadow: "rgb(0, 0, 0) 2px 0px 0px, rgb(0, 0, 0) 1.75517px 0.958851px 0px, rgb(0, 0, 0) 1.0806px 1.68294px 0px, rgb(0, 0, 0) 0.141474px 1.99499px 0px, rgb(0, 0, 0) -0.832294px 1.81859px 0px, rgb(0, 0, 0) -1.60229px 1.19694px 0px, rgb(0, 0, 0) -1.97999px 0.28224px 0px, rgb(0, 0, 0) -1.87291px -0.701566px 0px, rgb(0, 0, 0) -1.30729px -1.51361px 0px, rgb(0, 0, 0) -0.421592px -1.95506px 0px, rgb(0, 0, 0) 0.567324px -1.91785px 0px, rgb(0, 0, 0) 1.41734px -1.41108px 0px, rgb(0, 0, 0) 1.92034px -0.558831px 0px",
-                    top: "17%",
-                    width: "100%",
+                    fontSize: "2vh"
                 }}>
                 {(tokenType==="threat" || tokenType==="willpower" || tokenType==="attack" || tokenType==="defense" || tokenType==="hitPoints") && amount>0 ? "+"+amount : amount}
-            </p>
+            </div>
 
             <div
                 className="text-center text-sm"
@@ -148,7 +143,7 @@ export const Token = React.memo(({
                 />
             </div>
             <img 
-                className="block ml-auto mr-auto max-h-8"
+                className="block h-full"
                 src={process.env.PUBLIC_URL + '/images/tokens/'+tokenType+'.png'}
             />
         </div>
