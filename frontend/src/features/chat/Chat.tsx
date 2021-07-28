efd3cdf758d924fa033f456adae4604a24ace0d6
Chris Stanford <chris.j.stanford@gmail.com>
2021-07-28 17:06:56 -0400

    chat bugfix

--- frontend/src/features/chat/Chat.tsx
+++ frontend/src/features/chat/Chat.tsx
@@ -22,10 +22,10 @@ export const Chat: React.FC<Props> = ({ hover, chatBroadcast, setTyping }) => {
 
   return (
     <div className="overflow-hidden h-full">
-      <div className="bg-gray-800 overflow-y-auto" style={{height: "calc(100% - 3vh)"}}>
+      <div className="bg-gray-800 overflow-y-auto" style={{height: "calc(100% - 4vh)"}}>
         <ChatMessages hover={hover} chatOnly={chatOnly}/>
       </div>
-      <div className="text-center"  style={{height: "3vh"}}>
+      <div className="text-center"  style={{height: "4vh"}}>
         {isLoggedIn && <ChatInput chatBroadcast={chatBroadcast} setTyping={setTyping}/>}
       </div>
       <div className="absolute bottom-0 right-0 text-white p-1 cursor-default" style={{ opacity: "35%", paddingRight: "50px"}}>
