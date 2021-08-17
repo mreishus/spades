7b2f58851bbdee88ff32630e6cdefefd84ec81dc
Chris Stanford <chris.j.stanford@gmail.com>
2021-08-17 00:57:54 -0400

    fixed load game bug related to multiple instances of game in repo

--- backend/lib/dragncards_game/card.ex
+++ backend/lib/dragncards_game/card.ex
@@ -17,7 +17,6 @@ defmodule DragnCardsGame.Card do
 
   @spec card_from_cardrow(Map.t(), String.t()) :: Map.t()
   def card_from_cardrow(card_row, controller) do
-    IO.inspect(card_row)
     %{
       "id" => String.slice(Ecto.UUID.generate,24..-1),
       "rotation" => 0,
